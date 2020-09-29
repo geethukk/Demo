0fbc6381d18ce9dd07c9c85ce205b820eaea1d96
geethukk <geethu.kk@dreamorbit.com>
2020-09-29 16:07:03 +0530

    First commit on demo

---
+++ Demo/Adapter/TaskAdapter.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content.Res;
+using Android.Support.V7.Widget;
+using Android.Views;
+using System;
+namespace Demo.Adapter
+{
+    public class TaskAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<int> ItemClick;
+        public TaskDetails mTaskDetails;
+        public TaskAdapter(TaskDetails taskDetails)
+        {
+            mTaskDetails = taskDetails;
+        }
+        public override int ItemCount
+        {
+            get { return mTaskDetails.taskCount; }
+        }
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            TaskViewHolder vh = holder as TaskViewHolder;
+            vh.textViewWeekNumber.Text = mTaskDetails[position].mWeekNumber;
+            vh.textViewCodingType.Text = mTaskDetails[position].mCodingType;
+            vh.textViewStoreAddress.Text = mTaskDetails[position].mStoreAddress;
+            vh.textViewStoreName.Text = mTaskDetails[position].mStoreName;
+            vh.textViewWeekDate.Text = mTaskDetails[position].mWeekDate;
+            vh.buttonStartAudit.Visibility = mTaskDetails[position].mTaskstate.Equals("Not Started") ?ViewStates.Visible: ViewStates.Gone;
+        }
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.taskcard, parent, false);
+            TaskViewHolder vh = new TaskViewHolder(itemView, OnClick);
+            return vh;
+        }
+        private void OnClick(int obj)
+        {
+            if (ItemClick != null)
+                ItemClick(this, obj);
+        }
+    }
+}
---
+++ Demo/Adapter/Tasks.cs
@@ -0,0 +1,71 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using System;
+namespace Demo.Adapter
+{
+    public class Tasks
+    {
+        public string mWeekNumber { get; set; }
+        public string mWeekDate { get; set; }
+        public string mStoreName { get; set; }
+        public string mStoreAddress { get; set; }
+        public string mCodingType { get; set; }
+        public string mTaskstate { get; set; }
+
+    }
+    public class TaskDetails
+    {
+        static Tasks[] listTasks =
+        {
+            new Tasks() {mWeekNumber = "wk35", mWeekDate = "08-09",mStoreName="34492783/NEW RANDOLPH STORE 0045",mStoreAddress="630 3RD ST WEST, RANDOLPH AFB, TX, 78150",mCodingType="Item Coding",mTaskstate="Not Started"},
+            new Tasks() {mWeekNumber = "wk35", mWeekDate = "08-09",mStoreName="24492783/NEW RANDOLPH STORE 0045",mStoreAddress="530 3RD ST WEST, RANDOLPH AFB, TX, 78150",mCodingType="Item Coding",mTaskstate="Not Started"},
+            new Tasks() {mWeekNumber = "wk36", mWeekDate = "08-09",mStoreName="14492783/NEW RANDOLPH STORE 0045",mStoreAddress="430 3RD ST WEST, RANDOLPH AFB, TX, 78150",mCodingType="Item Coding",mTaskstate="In Progress"},
+            new Tasks() {mWeekNumber = "wk35", mWeekDate = "08-09",mStoreName="34492783/NEW RANDOLPH STORE 0045",mStoreAddress="630 3RD ST WEST, RANDOLPH AFB, TX, 78150",mCodingType="Item Coding",mTaskstate="Not Started"},
+            new Tasks() {mWeekNumber = "wk35", mWeekDate = "08-09",mStoreName="34492783/NEW RANDOLPH STORE 0045",mStoreAddress="630 3RD ST WEST, RANDOLPH AFB, TX, 78150",mCodingType="Item Coding",mTaskstate="Not Started"},
+            new Tasks() {mWeekNumber = "wk35", mWeekDate = "08-09",mStoreName="34492783/NEW RANDOLPH STORE 0045",mStoreAddress="630 3RD ST WEST, RANDOLPH AFB, TX, 78150",mCodingType="Item Coding",mTaskstate="Not Started"}
+           
+        };
+        private Tasks[] tasks;
+        Random random;
+        public TaskDetails()
+        {
+            this.tasks = listTasks;
+            random = new Random();
+        }
+        public int taskCount
+        {
+            get
+            {
+                return tasks.Length;
+            }
+        }
+        public Tasks this[int i]
+        {
+            get { return tasks[i]; }
+        }
+    }
+    public class TaskViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView textViewWeekNumber { get; set; }
+        public TextView textViewWeekDate { get; set; }
+        public TextView textViewStoreName { get; set; }
+
+        public TextView textViewStoreAddress { get; set; }
+
+        public TextView textViewCodingType { get; set; }
+        public Button buttonStartAudit { get; set; }
+
+        public TaskViewHolder(View itemview, Action<int> listener) : base(itemview)
+        {
+            textViewWeekNumber = itemview.FindViewById<TextView>(Resource.Id.textViewWeekNumber);
+            textViewWeekDate = itemview.FindViewById<TextView>(Resource.Id.textViewWeekDate);
+            textViewStoreName = itemview.FindViewById<TextView>(Resource.Id.textViewStoreName);
+            textViewStoreAddress = itemview.FindViewById<TextView>(Resource.Id.textViewStoreAddress);
+            textViewCodingType = itemview.FindViewById<TextView>(Resource.Id.textViewCodingType);
+            buttonStartAudit = itemview.FindViewById<Button>(Resource.Id.buttonStartAudit);
+
+            // itemview.Click += (sender, e) => listener(Position);
+        }
+    }
+}
